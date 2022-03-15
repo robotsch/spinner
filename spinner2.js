b8fba04468b0c98bf1f573a3da26b6410c2c0f22
robotsch <christianhumble22@gmail.com>
2022-03-15 12:34:22 -0400

    refactor spinner1 into spinner2

---
+++ spinner2.js
@@ -0,0 +1,4 @@
+let spinArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ']
+for(let i = 0; i < spinArr.length; i += 1) {
+  setTimeout(()=>{process.stdout.write(spinArr[i])}, i * 200)
+}
