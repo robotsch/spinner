c44bbf8bf654d1ccf6dabbd0db44bf437fb62d59
robotsch <christianhumble22@gmail.com>
2022-03-15 12:13:37 -0400

    add more frames to spin animation

--- spinner1.js
+++ spinner1.js
@@ -16,6 +16,40 @@ setTimeout(() => {
 }, 700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|   '); 
 }, 900);
+
+setTimeout(() => {
+
+  process.stdout.write('\r/   '); 
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   '); 
+}, 1200);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r/   '); 
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   '); 
+}, 2100);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 2300);
+
+setTimeout(() => {
+  process.stdout.write('\r\|   '); 
+}, 2500);
