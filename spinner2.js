3f17c9dc1674595fb17103550aed72ce5a8f1274
robotsch <christianhumble22@gmail.com>
2022-03-15 12:35:23 -0400

    lint code

--- spinner2.js
+++ spinner2.js
@@ -1,4 +1,6 @@
-let spinArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ']
-for(let i = 0; i < spinArr.length; i += 1) {
-  setTimeout(()=>{process.stdout.write(spinArr[i])}, i * 200)
+let spinArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   '];
+for (let i = 0; i < spinArr.length; i += 1) {
+  setTimeout(()=>{
+    process.stdout.write(spinArr[i]);
+  }, i * 200);
 }
